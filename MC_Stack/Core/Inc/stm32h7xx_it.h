15131c5edc237ba63659a63ac439e7a04fe4e2cc
MedveDev <medvedevdavid@gmail.com>
2023-01-08 20:39:48 +0100

    minor updates are added for generated code

--- MC_Stack/Core/Inc/stm32h7xx_it.h
+++ MC_Stack/Core/Inc/stm32h7xx_it.h
@@ -52,6 +52,7 @@ void MemManage_Handler(void);
 void BusFault_Handler(void);
 void UsageFault_Handler(void);
 void DebugMon_Handler(void);
+void SPI1_IRQHandler(void);
 void TIM8_UP_TIM13_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
